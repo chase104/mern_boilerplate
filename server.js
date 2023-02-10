c5b8d0d839e9c31d2ff001c5431af3098ff1f7c7
Chase Van Halen <cvanhalen@perscholas.org>
2023-02-10 11:04:57 -0600

    set up express

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+// cross origin access 
+const cors = require('cors');
+
+
+
+const app = express();
+
+// access
+app.use(cors({
+    origin: "*"
+}));
+
+// logs the different requests to our server
+app.use(logger('dev'))
+
+//parse stringified objects (JSON)
+app.use(express.json())
+
+// server build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
+app.listen(5000, () => {
+    console.log(`Server is Listening on 5000`)
+});
